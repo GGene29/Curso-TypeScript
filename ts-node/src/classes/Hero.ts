b25f6111e78f23e5e15209d564712df2d3d3b153
GGene29 <sumozacalcurian@gmail.com>
2024-11-01 16:08:52 -0400

    [ADD]-export e import

---
+++ ts-node/src/classes/Hero.ts
@@ -0,0 +1,10 @@
+// Las clases se tienden a iniciar con mayúscula, tanto en la declaración como en el archivo
+// Se agrega export, para que pueda usarse fuera del archivo
+export class Hero {
+    constructor(
+        name:string,
+        powerId: number,
+        age : number
+    ){}
+
+}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -4,8 +4,23 @@ console.log('Hola Mundo!!!');
 // Las dependencias se declaran en el package.json
 // No toque el package.lock.json
 
+// Uso de importaciones
+// import Clase from ruta
+import { Hero } from "./classes/Hero"; 
 
 
+const bakugo = new Hero("Dios de las explosiones", 5, 16);
+
+class NewHero extends Hero {
+    constructor(texto : string){
+        super("Todoroki" , 6,4)
+    }
+    
+}
+
+console.log({ bakugo });
+const heroe = new NewHero("Descripción")
+console.log(heroe);
 
 
 
